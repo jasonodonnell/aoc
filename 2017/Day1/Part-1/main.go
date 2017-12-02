723cd2493dc8c2bbc7c61191f4c98100d79370e4
Jason O'Donnell <odonnelljp01@gmail.com>
2017-12-02 17:16:29 -0500

    Add day 1

---
+++ 2017/Day1/Part-1/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+)
+
+var dat []byte
+var err error
+
+func init() {
+	filePath := flag.String("file", "../input.txt", "Path to input file")
+	flag.Parse()
+
+	dat, err = ioutil.ReadFile(*filePath)
+	if err != nil {
+		log.Fatalf("Could not read file: %s %s", err, *filePath)
+	}
+}
+
+func main() {
+	var captcha []int
+	var sum int
+
+	for _, v := range dat {
+		value, err := strconv.Atoi(string(v))
+		if err != nil {
+			continue
+		}
+		captcha = append(captcha, value)
+	}
+
+	stepSize := 1
+
+	for i := range captcha {
+		j := (i + stepSize) % len(captcha)
+		if captcha[i] == captcha[j] {
+			sum += captcha[i]
+		}
+	}
+	fmt.Println(sum)
+}
