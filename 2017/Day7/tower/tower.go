b93b40a398ca3ba0dcba8eb8c4804640d0e1c6a0
Jason O'Donnell <odonnelljp01@gmail.com>
2017-12-07 11:17:42 -0500

    Simplify findbase

--- 2017/Day7/tower/tower.go
+++ 2017/Day7/tower/tower.go
@@ -14,26 +14,19 @@ type Tower struct {
 }
 
 func (t *Tower) FindBase() string {
-	supports := make(map[string]int)
+Loop:
 	for _, v := range t.Bases {
-		if _, ok := supports[v]; !ok {
-			supports[v] = 0
-		}
 		for _, x := range t.Programs {
 			if x.Name == v || x.Supports == nil {
 				continue
 			}
 			for _, each := range x.Supports {
 				if v == each {
-					supports[v]++
+					continue Loop
 				}
 			}
 		}
-	}
-	for k, v := range supports {
-		if v == 0 {
-			return k
-		}
+		return v
 	}
 	return ""
 }
