2e85c425b25e6d43b4a58a5e805b63f7d5ef6d1a
Jason O'Donnell <odonnelljp01@gmail.com>
2017-12-10 02:30:25 -0500

    Add length to factory

--- 2017/Day10/knot/knot.go
+++ 2017/Day10/knot/knot.go
@@ -12,7 +12,7 @@ type Knot struct {
 
 func NewKnot(length int) *Knot {
 	var l []int
-	for i := 0; i < 256; i++ {
+	for i := 0; i < length; i++ {
 		l = append(l, i)
 	}
 	return &Knot{
@@ -51,12 +51,11 @@ func (k *Knot) Hash() string {
 	var hash string
 	for i < len(k.List) {
 		// Not sure how to do this on a slice.. hacky for now.
-		denseHash := int64(k.List[i] ^ k.List[i+1] ^
-			k.List[i+2] ^ k.List[i+3] ^ k.List[i+4] ^
-			k.List[i+5] ^ k.List[i+6] ^ k.List[i+7] ^
-			k.List[i+8] ^ k.List[i+9] ^ k.List[i+10] ^
-			k.List[i+11] ^ k.List[i+12] ^ k.List[i+13] ^
-			k.List[i+14] ^ k.List[i+15])
+		// Doing them two at a time didn't seem to work
+		denseHash := int64(k.List[i] ^ k.List[i+1] ^ k.List[i+2] ^ k.List[i+3] ^
+			k.List[i+4] ^ k.List[i+5] ^ k.List[i+6] ^ k.List[i+7] ^
+			k.List[i+8] ^ k.List[i+9] ^ k.List[i+10] ^ k.List[i+11] ^
+			k.List[i+12] ^ k.List[i+13] ^ k.List[i+14] ^ k.List[i+15])
 		hash += strconv.FormatInt(denseHash, 16)
 		i += 16
 	}
